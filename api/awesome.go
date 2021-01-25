29128989b0daa18d8ededc5e04db504f09074df3
Samuel Clark <samrocksc@gmail.com>
2021-01-25 09:03:25 +0100

    WINGZ N THINGZ

--- api/awesome.go
+++ api/awesome.go
@@ -6,5 +6,17 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	jokes := []string{
+		"I'm a big fan of whiteboards. I find them quite....re-markable.",
+		"I ate a clock yesterday, it was very time-consuming.",
+		"How did the picture end up in jail? It was framed!",
+		"How did the picture end up in jail? It was framed!",
+		"Cactus puns are simply succulent.",
+		"What do you call a tall pile of cats?......A meowntain",
+		"Why couldn't the bicycle stand up?....because it was too tired",
+		"What concert costs 45 cents? 50 cent ft. _Nickleback_",
+	}
+
+	fmt.Println(jokes)
 	fmt.Fprintf(w, "<h1>What did the buffalo say when his son left? Bison!</h1>")
 }
